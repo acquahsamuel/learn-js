242570b88ba4e50678167add1f013751d4901fe3
acquahsamuel <acquahsamuel1996@gmail.com>
2020-07-26 14:00:58 +0000

    lecture note

---
+++ 01-engineering-js-steel/index.js
@@ -0,0 +1,33 @@
+/* 
+
+Lecture Node 
+ Can i use - website 
+ ECMA - website 
+
+ Download google chrome 
+
+ MDN - Mozilla developer network
+ https://developer.mozilla.org/en-US/
+
+
+
+
+ Primitive Types 
+ Number = String = Boolean = Null = Undefined 
+ Other Technical object =  Symbol and BigInt 
+
+
+ Exponential 2 ** 3 
+ Addition 2 + 4 
+ Subtraction 2 -4 
+ Division  3 / 4 
+
+ Precedence (PEMDAS )
+ 
+
+*/
+
+
+
+
+

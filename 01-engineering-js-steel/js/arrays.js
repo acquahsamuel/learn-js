f131cd7a740bad88972c6b8c5889736933b24ffb
acquahsamuel <acquahsamuel1996@gmail.com>
2020-07-29 18:46:33 +0000

    arraymethods in js

--- 01-engineering-js-steel/js/arrays.js
+++ 01-engineering-js-steel/js/arrays.js
@@ -1,69 +1,97 @@
-const arrayValue = ['red','orange','blue','oink','yellow'];
+const arrayValue = [
+    'red',
+    'orange',
+    'blue',
+    'oink',
+    'yellow'
+];
 
 
-console.log(arrayValue[arrayValue.length -1]);
-console.log(arrayValue[arrayValue.length]= "Water")
+console.log(arrayValue[arrayValue.length - 1]);
+console.log(arrayValue[arrayValue.length] = "Water")
 
 console.log(arrayValue.length)
 console.log(arrayValue);
 
 
-
-let newMusic = [
-    'Theophilus Sunday',
-    'Hit man the jack',
-    'Amakye Dede song',
-    'Freedom by code'
-];
+let newMusic = ['Theophilus Sunday', 'Hit man the jack', 'Amakye Dede song', 'Freedom by code'];
 
 console.log(newMusic);
 
-letters = ['T','C','E','P','S','E','R'];
+letters = [
+    'T',
+    'C',
+    'E',
+    'P',
+    'S',
+    'E',
+    'R'
+];
 console.log(letters);
 console.log(letters.join('-'));
 
 
-let animals = ['shark','salmon','whale','bear','lizard','tortoise'];
+let animals = [
+    'shark',
+    'salmon',
+    'whale',
+    'bear',
+    'lizard',
+    'tortoise'
+];
 console.log(animals);
-// Remember the lenght is always -1 of the total 
-let swimmmers = animals.slice(0,3);
-console.log(swimmmers); 
+// Remember the lenght is always -1 of the total
+let swimmmers = animals.slice(0, 3);
+console.log(swimmmers);
 
-// Last item == -1 
-console.log(animals[animals.length -1]);
+// Last item == -1
+console.log(animals[animals.length - 1]);
 
 const quadruped = animals.slice(-3);
 console.log(quadruped);
 
 
 // Splice method on arrays
-console.log(animals.splice(1,1));
-console.log(animals.splice(1,1,'Herllo'))
+console.log(animals.splice(1, 1));
+console.log(animals.splice(1, 1, 'Herllo'))
 
 console.log(animals);
 
 
-let animals = [ 'shark', 'salmon', 'whale', 'bear', 'lizard', 'tortoise' ];
+let secondIndexOf = function (s1, s2) {
+    let firstIndex = s1.indexOf(s2);
+    return s1.indexOf(s2, firstIndex + 1);
+}
 
-//splice(startIdx, deleteCount, itemsToInsert);
+
+let animals = [
+    'shark',
+    'salmon',
+    'whale',
+    'bear',
+    'lizard',
+    'tortoise'
+];
+
+// splice(startIdx, deleteCount, itemsToInsert);
 
 // =====================
 // INSERTING w/ SPLICE
 // =====================
 // 'at index 1, delete 0 items and insert "octopus"'
 animals.splice(1, 0, 'octopus');
-//["shark", "octopus", "salmon", "whale", "bear", "lizard", "tortoise"]
+// ["shark", "octopus", "salmon", "whale", "bear", "lizard", "tortoise"]
 
 // =====================
 // DELETING w/ SPLICE
 // =====================
-//'at index 5, delete 2 items'
+// 'at index 5, delete 2 items'
 animals.splice(5, 2);
-//["shark", "octopus", "salmon", "whale", "bear"]
+// ["shark", "octopus", "salmon", "whale", "bear"]
 
 // =====================
 // REPLACING w/ SPLICE
 // =====================
-//'at index 3, delete 2 items and replace them with "orca" and "grizzly"'
+// 'at index 3, delete 2 items and replace them with "orca" and "grizzly"'
 animals.splice(3, 2, 'orca', 'grizzly');
-// ["shark", "octopus", "salmon", "orca", "grizzly"]
+// ["shark", "octopus", "salmon", "orca", "grizzly"]

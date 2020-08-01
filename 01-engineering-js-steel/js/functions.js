e0ea1ea974e41e650c6199b0f60edda1cb78139a
acquahsamuel <acquahsamuel1996@gmail.com>
2020-08-01 07:26:20 +0000

    random with array

--- 01-engineering-js-steel/js/functions.js
+++ 01-engineering-js-steel/js/functions.js
@@ -1,74 +1,103 @@
 function square(num) {
-  return num * num
+    return num * num
 }
 
 function addition(x, y) {
-  return x + y
+    return x + y
 }
 
 let squareS = square(3)
 console.log(squareS)
 
 function isPurple(color) {
-  return color.toLowerCase() === 'purple'
+    return color.toLowerCase() === 'purple'
 }
 
 console.log(isPurple('purple'))
 
 function containsPurple(arr) {
-  for (let color of arr) {
-    if (color === 'purple' || color === 'magenta') {
-      return true
+    for (let color of arr) {
+        if (color === 'purple' || color === 'magenta') {
+            return true
+        }
     }
-  }
-  return false
+    return false
 }
 
 // Function Challenge
 
 console.log('========Function Challenge ============')
+
 function isValidPassword(password, username) {
-  if (
-    password.length < 8 ||
-    password.indexOf(' ') !== -1 ||
-    password.indexOf(username) !== -1
-  ) {
-    return false
-  }
-  return true
+    if (password.length < 8 || password.indexOf(' ') !== -1 || password.indexOf(username) !== -1) {
+        return false
+    }
+    return true
 }
 
 console.log(isValidPassword('89Fjj1nms', 'dogluvr'));
 
 
-// function array challenge 
-function averageChallenge(arrayValu){
+// function array challenge
+function averageChallenge(arrayValu) {
     let total = 0;
-    for(let eachArrayVal of arrayValu){
-         total +=  eachArrayVal;
+    for (let eachArrayVal of arrayValu) {
+        total += eachArrayVal;
     }
     return total / arrayValu.length;
 }
 
-console.log(averageChallenge([0,50]));
+console.log(averageChallenge([0, 50]));
 
-// Pangrams 
-function isPangram(sentence){
-  let lowerCase = sentence.toLowerCase();
-   let alphabet = 'abcdefghijklmnopqrstuvwxyz';
-   for(let char of alphabet){
-       if(lowerCase.indexOf(char) === -1){
-         return false
-       }
-   }
-   return true;
+// Pangrams
+function isPangram(sentence) {
+    let lowerCase = sentence.toLowerCase();
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    for (let char of alphabet) {
+        if (lowerCase.indexOf(char) === -1) {
+            return false
+        }
+    }
+    return true;
 }
 
-isPangram();
+isPangram('Hellow');
 
 
-// Get Chard 
+// Get Chard
 
-function getChar(){
-  
+console.log('========getChar ============')
+
+
+function pick(arr) {
+    const idx = Math.floor(Math.random() * arr.length);
+    return arr[idx];
 }
+
+
+function getCard() {
+    let alphaNum = [
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        '10',
+        'J',
+        'Q',
+        'K',
+        'A'
+    ];
+
+    let suit = ['clubs', 'spades', 'hearts', 'diamonds'];
+    return {value: pick(alphaNum), suit: pick(suit)};
+
+}
+
+console.log(getCard());
+
+

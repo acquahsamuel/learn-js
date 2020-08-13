a99d55d15b6d383794e738fdf4f7f36c8da0fbbc
acquahsamuel <acquahsamuel1996@gmail.com>
2020-08-13 20:23:18 +0000

    deleted sheet

--- 01-engineering-js-steel/js/js-array.js
+++ 01-engineering-js-steel/js/js-array.js
@@ -43,6 +43,11 @@ console.log(unshitFruit);
 console.log(fruitJoin);
 
 
+// undefined and Null 
+
+/* Null is an intentional abscence of a value = Must be assigned */
+/* Undefined variable that do not have an assigned value are undefined */
+
 
 
 

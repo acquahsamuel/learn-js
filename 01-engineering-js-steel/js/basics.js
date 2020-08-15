05a7ebcc7c1544d3e9a142a16f6242b437d738c6
acquahsamuel <acquahsamuel1996@gmail.com>
2020-08-15 14:46:13 +0000

    function

--- 01-engineering-js-steel/js/basics.js
+++ 01-engineering-js-steel/js/basics.js
@@ -28,3 +28,19 @@ let numRef2 = numRef;
 
 
 
+
+console.log(Math.random(30));
+
+const area = 7.7;
+console.log(Math.round(area));
+
+// Round it one
+console.log(Math.floor(area));
+
+// Round it up 
+console.log(Math.ceil(area));
+
+// Turncate - Cut it down
+console.log(Math.trunc(area));
+
+
--- 01-engineering-js-steel/js/function.js
+++ 01-engineering-js-steel/js/function.js
@@ -3,8 +3,42 @@
 function definition(){
     console.log('Hello world');
     console.log('Hello Code');
-    console.log('Hello Javascript');
-    
+    console.log('Hello Javascript');   
 }
 
 definition();
+
+
+
+function rollDice(){
+    const randomDice = Math.floor(Math.random() * 6) + 1; 
+    console.log(randomDice);
+}
+
+rollDice();
+
+// Calling function inside (functions , conditionals , objects)
+function rollDie(){
+    rollDice();
+    rollDice();
+    rollDice();
+    rollDice();
+}
+
+
+function throwDice(rollTimes){
+   for(let i = 0 ; i < rollTimes ; i++){
+        rollDice();
+   }
+}
+
+//Multiple parameter function (Other matters)
+function sum(x , y){
+    console.log(x + y);
+}
+
+function divide(a , b){
+    console.log(a / b);
+}
+
+

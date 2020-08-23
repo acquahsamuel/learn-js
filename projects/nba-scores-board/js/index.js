3782cf1e74a0e77798e63e9904c425401e089c42
acquahsamuel <acquahsamuel1996@gmail.com>
2020-08-23 05:36:41 +0000

    gameboard

--- projects/nba-scores-board/js/index.js
+++ projects/nba-scores-board/js/index.js
@@ -82,27 +82,61 @@ const warriorsGames = [{
             isWinner: false
         }
     }
-]
+];
 
-const parentElement = document.createElement('ul');
+// **************************************************
+// STEP 2 - Refactored & Re-usable
+// **************************************************
 
-for (let game of warriorsGames) {
+const makeChart = (games, targetTeam) => {
+    const ulParent = document.createElement('ul');
+    for (let game of games) {
+        const gameLi = document.createElement('li');
+        gameLi.innerHTML = getScoreLine(game);
+        gameLi.classList.add(isWinner(game, targetTeam) ? 'win' : 'loss');
+        ulParent.appendChild(gameLi);
+    }
+    return ulParent;
+};
+
+const isWinner = ({
+    homeTeam,
+    awayTeam
+}, targetTeam) => {
+    const target = homeTeam.team === targetTeam ? homeTeam : awayTeam;
+    return target.isWinner;
+};
+
+const getScoreLine = ({
+    homeTeam,
+    awayTeam
+}) => {
     const {
-        awayTeam,
-        homeTeam
-    } = game;
+        team: hTeam,
+        points: hPoints
+    } = homeTeam;
+    const {
+        team: aTeam,
+        points: aPoints
+    } = awayTeam;
+    const teamNames = `${aTeam} @ ${hTeam}`;
+    let scoreLine;
+    if (aPoints > hPoints) {
+        scoreLine = `<b>${aPoints}</b>-${hPoints}`;
+    } else {
+        scoreLine = `${aPoints}-<b>${hPoints}</b>`;
+    }
+    return `${teamNames} ${scoreLine}`;
+};
 
-    const gameLi = document.createElement('li');
-    
+//Select the 2 sections to append to (from index.html)
+const gsSection = document.querySelector('#gs');
+const houstonSection = document.querySelector('#hr');
 
-    const gameTeam = `${awayTeam.team} @ ${homeTeam.team}`;
-    const gamePoints = `${awayTeam.points} - ${homeTeam.points}`;
+// Make the 2 charts:
+const gsChart = makeChart(warriorsGames, 'Golden State');
+const hrChart = makeChart(warriorsGames, 'Houston');
 
-    gameLi.innerText = `${gameTeam}, ${gamePoints}`;
-
-    parentElement.appendChild(gameLi);
-
-
-}
-
-document.body.prepend(parentElement);
+//Append them!
+gsSection.appendChild(gsChart);
+houstonSection.appendChild(hrChart);
